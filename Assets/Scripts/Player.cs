978bf989742fcbd88258da4a156fdcedca5101f1
Shane Walsh <swalsh818@gmail.com>
2022-09-01 10:55:17 +0100

    Jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,40 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 15f;
 
     float controlThrow;
     Rigidbody2D myRigidBody2D;
     Animator myAnimator;
+    BoxCollider2D myBoxCollider2D;
+    PolygonCollider2D myPlayersFeet;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        myPlayersFeet = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if(!myPlayersFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        bool isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+        if(isJumping)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidBody2D.velocity.x, jumpSpeed);
+            myRigidBody2D.velocity = jumpVelocity;
+        }
     }
 
     private void Run()
