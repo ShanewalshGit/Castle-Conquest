bbe02aa329250afbea4c255722386aaa104df845
Shane Walsh <swalsh818@gmail.com>
2022-09-02 10:57:50 +0100

    Improved Climbing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     BoxCollider2D myBoxCollider2D;
     PolygonCollider2D myPlayersFeet;
 
+    float startingGravityScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,8 @@ public class Player : MonoBehaviour
         myAnimator = GetComponent<Animator>();
         myBoxCollider2D = GetComponent<BoxCollider2D>();
         myPlayersFeet = GetComponent<PolygonCollider2D>();
+
+        startingGravityScale = myRigidBody2D.gravityScale;
     }
 
     // Update is called once per frame
@@ -34,7 +38,7 @@ public class Player : MonoBehaviour
 
     private void Climb()
     {
-        if(myPlayersFeet.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        if(myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
             float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
             Vector2 climbingVelocity = new Vector2(myRigidBody2D.velocity.x, controlThrow * climbingSpeed);
@@ -42,10 +46,13 @@ public class Player : MonoBehaviour
             myRigidBody2D.velocity = climbingVelocity;
 
             myAnimator.SetBool("Climbing", true);
+
+            myRigidBody2D.gravityScale = 0f;
         }
         else
         {
             myAnimator.SetBool("Climbing", false);
+            myRigidBody2D.gravityScale = startingGravityScale;
         }
     }
 
